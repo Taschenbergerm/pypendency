4711d1c6ad316c1dd0eb7590d051def8fc1380eb
marvin.taschenberger <marvin.taschenberger@solactive.com>
2021-04-10 23:14:52 +0200

    fixed wrong import of GenericType to acctual implementation and lowered dependency from graph to nodes

--- pypendency/lexer/lark/lexer.py
+++ pypendency/lexer/lark/lexer.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from pypendency.models.generics import Relation
+from pypendency.models.generics import BaseNode
 from .core import Lexer
 from .transformer import Transformer
 
@@ -11,6 +12,9 @@ class RelationLexer(object):
     ):
         self.lexer = lexer
         self.transformer = transformer
+
+    def set_nodes(self, nodes: List[BaseNode]):
+            self.transformer.set_nodes(nodes)
 
     def lex(self, raw_relations: List[str]) -> List[Relation]:
         relations = []
