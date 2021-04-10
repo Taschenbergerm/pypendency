6cb82a1d8b68dff6bf924dd9773c24c8193c0b9f
marvin.taschenberger <marvin.taschenberger@solactive.com>
2021-04-10 23:17:13 +0200

    fixed bug related to scope and graph and imports

--- pypendency/parser/yaml/parse.py
+++ pypendency/parser/yaml/parse.py
@@ -2,7 +2,8 @@ import pathlib
 from typing import Union
 
 import yaml
-from pypendency.models.graph import Graph, BaseNode
+from pypendency.models.graph import Graph
+from pypendency.models.generics import BaseNode
 from pypendency.generics import LexerInterface
 
 
@@ -24,21 +25,23 @@ class Parser(object):
 
     def parse_string(self, content: str) -> Graph:
         ctx = yaml.load(content)
-        graph = Graph(ctx["id"])
+        graph = Graph(ctx["project"]["id"])
         self._create_nodes_in_graph(ctx["owned"], graph, external=False)
         self._create_nodes_in_graph(ctx["external"], graph, external=True)
+        self.lexer.set_nodes(graph.nodes)
         relations = self.lexer.lex(ctx["relations"])
+        graph.attach_relations(relations)
+        return graph
 
-
-def _create_nodes_in_graph(self, raw_nodes, graph, external):
-    with graph:
-        for slug, node in raw_nodes.items():
-            node[slug] = BaseNode(
-                node["name"],
-                slug=slug,
-                type=node["type"],
-                domain=node["domain"],
-                id=node["id"],
-                global_node=external,
-                description=node.get("description"),
-            )
+    def _create_nodes_in_graph(self, raw_nodes, graph, external):
+        with graph:
+            for slug, node in raw_nodes.items():
+                node[slug] = BaseNode(
+                    node["name"],
+                    slug=slug,
+                    type=node["type"],
+                    domain=node["domain"],
+                    id=node["id"],
+                    external=external,
+                    description=node.get("description"),
+                )
